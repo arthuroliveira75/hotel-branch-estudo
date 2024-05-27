869fc53661b2b5ee02f431b36be25852285316ce
Arthur Oliveira <ath.ur@live.com>
2024-05-27 20:23:08 -0300

    Commit 27/05/2024

---
+++ UI/AngularCurse.ts
@@ -0,0 +1,44 @@
+// Angular HTTP and Observables
+
+// Introduction - In enterprise apps, you need to interact with APIs.
+// You may be familiar with ajax, fetch.
+// HttpClient is Service Provided by Angular to interact with APIs.
+// HttpClient internally uses RxJs.
+
+// Setting Up HttpClient - 
+// We need to import HttpClientModule
+// Once module is imported we can inject HttpClient Service.
+
+// 7:58:33
+
+// RxJs and Observables
+
+// - RxJs is an library for writing Reactive Programming.
+// - Observables are stream of data to which we can subscribe.
+
+
+// API Interaction
+// - Performing CRUD operation using HttpCliente.
+// - Error Handling
+
+// 8:24:39
+
+// RxJs Operators - ShareReplay, CatchError, Map operators
+
+//Built-in Pipes - DatePipe, UpperCasePipe, LowerCasePipe,
+//CurrencyPipe, DecimalPipe, PercentPipe, JsonPipe, SlicePipe,
+//AsyncPipe
+
+//8:48:00
+
+//RxJs Operators - SharePlay, CatchError, Map operators(Angain)
+
+//9:09:42 Http Intercpetors and APP_INITIALIZER - 
+// Allows us to intercept request, 
+// We can change data and add headers,
+// APP_INITIALIZER allows you to inject functions as application startup.
+
+//9:24:02 - HttpIntercpetors
+
+
+//9:30:19 -
